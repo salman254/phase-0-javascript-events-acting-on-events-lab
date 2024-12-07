1380309d5a537316f9dbbb55f067620dee47c33f
Salman <salmaanmohamed700@gmail.com>
2024-12-07 23:46:37 +0300

    Moving Things with JavaScript by Acting on Events

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+dodger.style.position = 'absolute';
+dodger.style.bottom = '0px';  
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left, 10); 
+  if (left > 0) { 
+    dodger.style.left = `${left - 1}px`; 
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left, 10); 
+  if (left < 360) { 
+    dodger.style.left = `${left + 1}px`; 
+  }
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') { 
+    moveDodgerLeft(); 
+  } else if (e.key === 'ArrowRight') { 
+    moveDodgerRight(); 
+  }
+});
